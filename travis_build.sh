fe0f34c20580e21f5c3f1d6efa5bd9ce028ee735
Ben Coman <btc@openInWorld.com>
2019-01-12 01:06:30 +0800

    mend

--- travis_build.sh
+++ travis_build.sh
@@ -4,18 +4,20 @@ env | sort
 
 if [[ "${PRODUCTION}" -ne "" ]]; then
   echo PRODUCTION BUILD
-  sleep 5
+#  sleep 5
 fi
 echo BUILDING... ${TRAVIS_OS_NAME}${BUILD}
 
-if [[ "${TESTIMAGE}" -eq "Squeak32-4.6" ]]; then
+if [[ "x${TESTIMAGE}" == "xSqueak32-4.6" ]]; then
+  echo SQUEAK 4.6
   exit 1
 fi
 
-if [[ "${FAIL}" -eq "OK" ]]; then
+if [[ "x${FAIL}" == "xOK" ]]; then
+  echo FAIL OK
   exit 1
 fi
 
-sleep 5
+#sleep 5
 #  - find / -maxdepth 2 -print -name proc -prune || true
 #  - find /proc -maxdepth 2
