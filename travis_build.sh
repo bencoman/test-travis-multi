8bbcd7e1d7e6435d415491cb692def7b67fb4f82
Ben Coman <btc@openInWorld.com>
2019-01-11 23:43:46 +0800

    JSON style .travis.yml

--- travis_build.sh
+++ travis_build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -x
-if [[ "${PRODUCTION}" -neq "" ]]; then
-  echo PRODUCTION BUILD
-fi
-echo BUILDING... ${TRAVIS_OS_NAME}${BUILD}
 uname -a
 env | sort
+if [[ "${PRODUCTION}" -ne "" ]]; then
+  echo PRODUCTION BUILD
+  sleep 5
+fi
+echo BUILDING... ${TRAVIS_OS_NAME}${BUILD}
+sleep 5
 #  - find / -maxdepth 2 -print -name proc -prune || true
 #  - find /proc -maxdepth 2
