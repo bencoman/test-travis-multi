2df0f027983aaceea936abe77614f4deff1844d8
Ben Coman <btc@openInWorld.com>
2019-01-12 00:59:36 +0800

    try failures

--- travis_build.sh
+++ travis_build.sh
@@ -1,11 +1,21 @@
 #!/bin/sh -x
 uname -a
 env | sort
+
 if [[ "${PRODUCTION}" -ne "" ]]; then
   echo PRODUCTION BUILD
   sleep 5
 fi
 echo BUILDING... ${TRAVIS_OS_NAME}${BUILD}
+
+if [[ "${TESTIMAGE}"="Squeak32-4.6" ]]; then
+  exit 1
+fi
+
+if [[ "${FAIL}"="OK" ]]; then
+  exit 1
+fi
+
 sleep 5
 #  - find / -maxdepth 2 -print -name proc -prune || true
 #  - find /proc -maxdepth 2
