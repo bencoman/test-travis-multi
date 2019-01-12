a5b898e16da43ca42e4de713a3fdb777884ea11a
Ben Coman <btc@openInWorld.com>
2019-01-12 14:41:37 +0800

    Allow failures EXPERIMENTAL

--- travis_build.sh
+++ travis_build.sh
@@ -2,21 +2,43 @@
 uname -a
 env | sort
 
-if [ "${PRODUCTION}" -ne "" ]; then
-  echo PRODUCTION BUILD
-#  sleep 5
-fi
-echo BUILDING... ${TRAVIS_OS_NAME}${BUILD}
-
-if [ "x${TESTIMAGE}" = "xSqueak32-4.6" ]; then
-  echo SQUEAK 4.6
+# 
+if [ -n "${TESTFAIL}" ]; then
+  echo SIMULATE FAILURE
+ exit 1
 fi
 
-if [ "x${FAIL}" = "xOK" ]; then
-  echo FAIL OK
-  exit 1
+echo Current build parameters... "stage:${TRAVIS_BUILD_STAGE_NAME},os:${TRAVIS_OS_NAME},env:BUILD=${BUILD}"
+PRODUCTION_PARAMS="stage:Production,os:${TRAVIS_OS_NAME},env:BUILD=${BUILD}"
+echo Check for matching build... "${PRODUCTION_PARAMS}"
+echo Found...
+# If there is a matching job defined in the Production stage...
+if cat .travis.yml | tr -d '"[:blank:]' | grep ${PRODUCTION_PARAMS}; then
+  # and we are not in the Production stage...
+  if [ "$TRAVIS_BUILD_STAGE_NAME" != "Production" ]; then
+    # don't don't build again.
+    echo ALREADY BUILT BY PRODUCTION STAGE, SKIPPING...
+    exit 1
+  fi
+else
+  echo nothing... no matching production build
 fi
 
-#sleep 5
-#  - find / -maxdepth 2 -print -name proc -prune || true
-#  - find /proc -maxdepth 2
+CPU=$(echo ${BUILD} | cut -d/ -f1)
+FLAVOR=$(echo ${BUILD} | cut -d/ -f2)
+ARCH=${TRAVIS_OS_NAME}${CPU}
+which travis
+echo BUILDING... build.${ARCH}/${FLAVOR}
+pwd
+gem install travis
+find / -name travis 2>/dev/null
+exit 1
+
+
+# PLACEHOLDER BUILD
+#find /home -maxdepth 4
+#find / -maxdepth 2 -print -name proc -prune -o -name dev -prune -o -name Volume -prune || true
+#if find /proc -maxdepth 2 ; then
+#   echo DONE
+#fi
+
