7c95d20eb30da3e8fb98782ca4806feb34df5708
Ben Coman <btc@openInWorld.com>
2019-01-14 20:51:33 +0800

    slim try agin

--- travis_cancel_myself.sh
+++ travis_cancel_myself.sh
@@ -2,9 +2,9 @@
 
 JOB=`echo $TRAVIS_JOB_WEB_URL | rev | cut -d/ -f1 | rev`
 
-# The following wrapper prevents code tracing with `set -x` from revealling secret token
+# The following wrapper prevents code tracing with `set -x` from revealling the secret token
 { curl -X POST \
      -H "Travis-API-Version: 3" \
-     -H "Authorization: token $UI_CONFIG_SECRET_TOKEN" \
+     -H "Authorization: token $UI_CONFIGURED_SECRET_TOKEN" \
      https://api.travis-ci.org/job/$JOB/cancel 
 } 2> /dev/null
